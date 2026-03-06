bdc011b4d5b886becd1ee20bb9615b6e13de417a
0euun <jeyejong70@naver.com>
2026-03-06 17:59:53 +0900

    🔨 Post 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+LANGUAGE_CHOICES = (
+    (1, "KOR"),
+    (2, "ENG"),
+    (3, "JPN"),
+    (4, "CHN"),
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    language = models.IntegerField(choices=LANGUAGE_CHOICES)
+
+    def __str__(self):
+        return self.title
