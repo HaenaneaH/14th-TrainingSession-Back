0e84c188f9a5511ec0f6f126601e4a751e53bf92
0euun <jeyejong70@naver.com>
2026-03-06 18:52:52 +0900

    ✨ Post get 구현

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpRequest, Http404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostList(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request:HttpRequest, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
